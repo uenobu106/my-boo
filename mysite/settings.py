bc93a22afc6963ec230b7556974eadbc93891580
nobuhiro.ueno <uenobu106@gmail.com>
2020-01-20 00:40:42 +0900

    setting.pyの修正

--- mysite/settings.py
+++ mysite/settings.py
@@ -129,3 +129,6 @@ LOGIN_REDIRECT_URL = '/'
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
